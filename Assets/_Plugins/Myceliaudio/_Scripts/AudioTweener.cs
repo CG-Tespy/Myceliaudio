5964fc71e0d392c5a420240c2aa94818d7d544c4
CGTIBPDESKTOP\gbcab <gbcabrera@live.com>
2024-10-20 01:45:58 -0400

    Removed several unused scripts | Reorganized files

--- Assets/_Plugins/Myceliaudio/_Scripts/AudioTweener.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Myceliaudio
-{
-    public abstract class AudioTweener : ScriptableObject, IAudioTweener
-    {
-        public abstract void FadeVol(AudioTweenArgs args);
-    }
-}
